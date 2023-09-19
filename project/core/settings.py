8b85efff3c794407ff61e5b9f825499ec3b96b79
Chris Berry <chrisdberry@protonmail.com>
2023-09-19 09:25:20 +0200

    project/core/settings.py Bug fix

--- project/core/settings.py
+++ project/core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import keys
+from . import keys
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
